5cdbe9cca664cae34542f19787c42f0fb55970c9
Bitazee <katalenasm2@mailbox.winthrop.edu>
2021-02-10 12:03:23 -0500

    Renamed files from Alumni to Student

--- yearUpProject/src/com/yearup/YearUpControllerServlet.java
+++ yearUpProject/src/com/yearup/YearUpControllerServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class YearUpControllerServlet
  */
-@WebServlet("/YearUpControllerServlet")
+@WebServlet("/YearUpControllerServlet/*")
 public class YearUpControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Student> studentList = new ArrayList<Student>();
